83b17dbb72a7596ba7e95f256a1f9d28aa8c5525
karmilan <delftkarmilan@gmail.com>
2024-08-25 21:15:46 +0530

    cv and experience updated

--- src/components/topbar/TopContent.jsx
+++ src/components/topbar/TopContent.jsx
@@ -1,48 +1,55 @@
-import React from 'react'
-import {Grid, Typography} from '@mui/material'
-import {globalStyle} from '../../style/GlobalStyles'
-import {topBarStyle} from './TopBarStyle'
-import Button from '@mui/material/Button';
-import resume from '../../assets/Resume-Karmilan-FullStack-v1.pdf'
+import { Grid, Typography } from "@mui/material";
+import Button from "@mui/material/Button";
+import React from "react";
+import { globalStyle } from "../../style/GlobalStyles";
+import { topBarStyle } from "./TopBarStyle";
 
 const TopContent = () => {
-    const resume = 'https://bitly.ws/Ymxr'
-    return (
-        <>
-            <Grid sx={
-                topBarStyle.topContent
-            }>
-                <Typography component='h1'
-                    sx={
-                        globalStyle.textColor
-                    }
-                    fontSize={{xs:'20px', md:'30px'}}>Mr. MAHENDRARASA KARMILAN</Typography>
-                <Typography component='p'
-                    sx={
-                        globalStyle.textColor
-                }>A professional Software Developer</Typography>
+  const resume = "https://shorturl.at/dekrf";
+  return (
+    <>
+      <Grid sx={topBarStyle.topContent}>
+        <Typography
+          component="h1"
+          sx={globalStyle.textColor}
+          fontSize={{ xs: "20px", md: "30px" }}
+        >
+          Mr. MAHENDRARASA KARMILAN
+        </Typography>
+        <Typography component="p" sx={globalStyle.textColor}>
+          A professional Software Developer
+        </Typography>
 
-                <Grid container
-                    mt={{xs:3, md:4}}>
-                    
-                    <Button href={resume} size="large" variant="contained"
-                        sx={{
-                            ...globalStyle.secondaryBgColor, '&:hover':{backgroundColor:'#1a0b2ed1'}
-                    }}>Dowload CV</Button>
+        <Grid container mt={{ xs: 3, md: 4 }}>
+          <Button
+            href={resume}
+            size="large"
+            variant="contained"
+            sx={{
+              ...globalStyle.secondaryBgColor,
+              "&:hover": { backgroundColor: "#1a0b2ed1" },
+            }}
+          >
+            Dowload CV
+          </Button>
 
-                    <Button href='https://github.com/karmilan' size='large' variant="outlined"
-                        sx={
-                            {
-                                ...globalStyle.textColor,
-                                borderColor: '#ffff',
-                                ml: 2, '&:hover':{borderColor:'#ffff'}
-                            }
-                    }>my work</Button>
+          <Button
+            href="https://github.com/karmilan"
+            size="large"
+            variant="outlined"
+            sx={{
+              ...globalStyle.textColor,
+              borderColor: "#ffff",
+              ml: 2,
+              "&:hover": { borderColor: "#ffff" },
+            }}
+          >
+            my work
+          </Button>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
 
-                </Grid>
-            </Grid>
-        </>
-    )
-}
-
-export default TopContent
+export default TopContent;
